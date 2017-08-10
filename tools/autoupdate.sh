dee533f9338987ffb6face132d42b9086b4dc8e1
yulei <yulei.history@gmail.com>
2017-08-10 17:25:52 +0800

    修改shell

--- tools/autoupdate.sh
+++ tools/autoupdate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #mygit='https://github.com/yulei88/autohosts'
-myhost='/home/yulei/autohosts/data/hosts'
+myhostsupdate='/home/yulei/autohosts/tools/hostsupdate.sh'
+myhostslist='/home/yulei/autohosts/data/hosts.lst'
+myhosts='/home/yulei/autohosts/data/hosts'
+
+shpath=$(cd `dirname $0`; pwd)
+cd $shpath
+
+$myhostsupdate $myhostslist > $myhosts
 
 mycomment=`date`
 

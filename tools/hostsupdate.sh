9860253a91d20c751a7a8295a3aeab76b1dd6948
yulei <yulei.history@gmail.com>
2017-08-10 16:36:49 +0800

    Init

---
+++ tools/hostsupdate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage: hostsupdate ../data/hosts.lst
+
+hostslist="$1"                               # 参考的host来源
+
+cat $hostslist | while read line; do
+    if [[ ${line:0:1} == '#' ]] || [[ ${#line} == 0 ]] \
+	        || [[ $(echo $line | grep localhost) != "" ]] \
+	        || [[ $(echo $line | grep $HOSTNAME) != "" ]]; then
+        echo $line
+    else
+        link=$(nslookup "$line" | sed '/^$/d' | sed -n '$p' | sed -n 's/Address: //gp')
+        if [[ "$link" != "" ]]; then
+            printf "%-19s%s\n" $link $line
+        fi
+    fi
+done
+echo '#Last updated: ' `date`

a634f89e8adffe40b2c9e748a60f211ce5911ded
yulei <yulei@china-qizhi.com>
2018-10-26 09:44:54 +0800

    修改

--- tools/hostsupdate.sh
+++ tools/hostsupdate.sh
@@ -9,8 +9,8 @@ echo '127.0.0.1	localhost'
 echo '::1     localhost ip6-localhost ip6-loopback'
 cat $hostslist | while read line; do
     if [[ ${line:0:1} == '#' ]] || [[ ${#line} == 0 ]] \
-	        || [[ $(echo $line | grep localhost) != "" ]] \
-	        || [[ $(echo $line | grep $HOSTNAME) != "" ]]; then
+	        || [[ $(echo $line | grep localhost) != "" ]]; then
+#	        || [[ $(echo $line | grep $HOSTNAME) != "" ]]; then
         echo $line
     else
         link=$(nslookup "$line" | sed '/^$/d' | sed -n '$p' | sed -n 's/Address: //gp')
